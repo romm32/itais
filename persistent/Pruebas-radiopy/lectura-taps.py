5d5bfd0d5f3eab9193d395310e0bb5234e3a3331
Romina Garcia <rominag@fing.edu.uy>
2024-04-08 16:43:46 -0300

    Update 3 files
    - /persistent/Pruebas-radiopy/taps.txt
    - /persistent/Pruebas-radiopy/lectura-taps.py
    - /persistent/Pruebas-radiopy/prueba-radiopy.py

--- persistent/Pruebas-radiopy/lectura-taps.py
+++ persistent/Pruebas-radiopy/lectura-taps.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-# Open the file for reading in binary mode
+# Se abre el archivo generado, se convierten las muestras a complejos y se imprimen.
 with open("taps.txt", "rb") as file:
     # Read the bytes from the file
     taps_bytes = file.read()
 
-# Convert the bytes back to a NumPy array of complex numbers
 taps = np.frombuffer(taps_bytes, dtype=np.complex64)
 print(taps[:10])
-# Now you can work with the 'taps' array, which contains the complex numbers
 
--- persistent/Pruebas-radiopy/prueba-radiopy.py
+++ persistent/Pruebas-radiopy/prueba-radiopy.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# zmq_REQ_REP_server.py
-
-
 import pmt
 import zmq
 import numpy as np
 
-_debug = 0          # set to zero to turn off diagnostics
 
-# Set up ZMQ SUB socket
+# Se inicializa un socket TCP para recibir muestras de radio.py
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")  # Connect to the same port as in your GNU Radio script
+socket.connect("tcp://localhost:5555") 
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
 i = 0
 
@@ -21,21 +17,16 @@ with open("taps.txt", "wb") as file:
     try:
         while True:
             print('hello')
-            # Receive data from ZMQ socket
             data = socket.recv()
             print('middletop')
-            # Convert bytes to a NumPy array of complex samples
             taps = np.frombuffer(data, dtype=np.complex64)
             print('middle')
             if (i < 8):
-            	# Write the taps to the file
-            	#file.write(taps.tobytes())
             	file.write(taps)
             	i += 1
             print('goodbye')
     except KeyboardInterrupt:
         pass
 
-# Clean up the ZMQ context
 socket.close()
 context.term()
