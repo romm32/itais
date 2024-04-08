65000a0a17517189aee64c5f0e2fe0260d5fa14a
Romina Garcia <rominag@fing.edu.uy>
2024-04-08 13:22:55 -0300

    Update 9 files
    - /persistent/Prueba-bloqueGNU/__pycache__/epy_block_0.cpython-38.pyc
    - /persistent/Pruebas_bloque-sub_gps/__pycache__/epy_block_0.cpython-38.pyc
    - /persistent/Pruebas_GPS/__pycache__/sub_gps.cpython-38.pyc
    - /persistent/Prueba-bloqueGNU/pruebabloque.py
    - /persistent/Pruebas_bloque-sub_gps/bloquesubgps.py
    - /persistent/Pruebas-buffer/tapsfrombuffer.txt
    - /persistent/Prueba-bloqueGNU/comousar.txt
    - /persistent/Pruebas-buffer/pruebas-buffer.py
    - /persistent/README.md

--- persistent/Pruebas-buffer/pruebas-buffer.py
+++ persistent/Pruebas-buffer/pruebas-buffer.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# zmq_REQ_REP_server.py
-
-
 import pmt
 import zmq
 import numpy as np
 
-_debug = 0          # set to zero to turn off diagnostics
-
-# Set up ZMQ SUB socket
+# Se inicializa un socket
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")  # Connect to the same port as in your GNU Radio script
+socket.connect("tcp://localhost:5555")  # Debe ser el mismo puerto utilizado en GNU Radio.
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
 i = 
 
@@ -21,23 +16,19 @@ with open("tapsfrombuffer.txt", "wb") as file:
     try:
         while True:
             print('hello')
-            # Receive data from ZMQ socket
             data = socket.recv()
             print('middletop')
-            # Convert bytes to a NumPy array of complex samples
             taps = np.frombuffer(data, dtype=np.complex64)
             print('middle')
             if (i < 8):
             	print('goodbye')
-            	# Write the taps to the file
+            	# Se escriben las taps en el archivo creado.
             	file.write(taps)
             	print(np.size(taps))
             	i += 1
-            #print('goodbye')
     except KeyboardInterrupt:
         pass
 
-# Clean up the ZMQ context
 socket.close()
 context.term()
 
---
+++ persistent/README.md
@@ -0,0 +1,4 @@
+#### aclarar que algunas de las pruebas en esta carpeta no funcionan actualmente, yo pondria aca en el
+readme una lista de las listas que fueron hechas para versiones previas de los bloques y eso,
+y otras que sí se pueden hacer (pensando sobre todo en que las pruebas del gps son útiles hoy en día,
+no sé si muchas otras pero esas re sí)
