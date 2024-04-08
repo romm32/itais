e91d7e2e9df7fd60eac7e0a7d5d5bc983a9cc9c4
Romina Garcia <rominag@fing.edu.uy>
2024-04-08 15:41:31 -0300

    Update 3 files
    - /persistent/Pruebas_Pub-Sub/Prueba_SUB/prueba_SUB.py
    - /persistent/Pruebas_Pub-Sub/sub-prueba.py
    - /persistent/Pruebas_Pub-Sub/como-usar.txt

--- persistent/Pruebas_Pub-Sub/sub-prueba.py
+++ persistent/Pruebas_Pub-Sub/sub-prueba.py
@@ -1,44 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# zmq_REQ_REP_server.py
-
-# This server program capitalizes received strings and returns them.
-# NOTES:
-#   1) To comply with the GNU Radio view, messages are received on the REQ socket and sent on the REP socket.
-#   2) The REQ and REP messages must be on separate port numbers.
-
 import pmt
 import zmq
 import numpy as np
 
-_debug = 0          # set to zero to turn off diagnostics
 
-# Set up ZMQ SUB socket
+# Se inicializa un socket
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")  # Connect to the same port as in your GNU Radio script
+socket.connect("tcp://localhost:5555") # Debe ser la misma IP:puerto que se utilice en el flowgraph
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
-#contador = 0
 
 with open("taps.txt", "wb") as file:
     try:
         while True:
-            #contador += 1
+            # Se reciben datos, se interpretan como complejos y se imprimen en el archiov taps creado.
+            # Se tienen varios prints para debug.
             print('hello')
-            # Receive data from ZMQ socket
             data = socket.recv()
             print('middletop')
-            # Convert bytes to a NumPy array of complex samples
             taps = np.frombuffer(data, dtype=np.complex64)
             print('middle')
-            # Write the taps to the file
             file.write(taps.tobytes())
             print('goodbye')
     except KeyboardInterrupt:
         pass
 
-# Clean up the ZMQ context
 socket.close()
 context.term()
