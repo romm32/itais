b2dd0816b4896f0531fbcc0b0c0cdd2544af560c
Romina Garcia <rominag@fing.edu.uy>
2024-04-08 16:06:04 -0300

    Update 3 files
    - /persistent/Pruebas-pasaje-diccionarios/__pycache__/epy_block_0.cpython-38.pyc
    - /persistent/Pruebas-pasaje-diccionarios/pruebasdic.py
    - /persistent/Pruebas-pasaje-diccionarios/epy_block_0.py

--- persistent/Pruebas-pasaje-diccionarios/epy_block_0.py
+++ persistent/Pruebas-pasaje-diccionarios/epy_block_0.py
@@ -1,35 +1,26 @@
 """
-Embedded Python Blocks:
-
-Each time this file is saved, GRC will instantiate the first class it finds
-to get ports and parameters of your block. The arguments to __init__  will
-be the parameters. All of them are required to have default values!
+Bloque que se subscribe a un socket TCP desde el cual se recibirá información GPS.
 """
 
 import numpy as np
 from gnuradio import gr
 import zmq
 
-class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
-    """Embedded Python Block example - a simple multiply const"""
+class blk(gr.sync_block):  
 
-    def __init__(self, example_param=1.0):  # only default arguments here
-        """arguments to this function show up as parameters in GRC"""
+    def __init__(self, example_param=1.0): 
         gr.sync_block.__init__(
             self,
-            name='Embedded Python Block',   # will show up in GRC
-            in_sig=[],
+            name='Embedded Python Block',
             out_sig=[np.complex64]
         )
-        # if an attribute with the same name as a parameter is found,
-        # a callback is registered (properties work, too).
+
         self.example_param = example_param
 
     def work(self, input_items, output_items):
-        """example: multiply with constant"""
         context = zmq.Context()
         subscriber = context.socket(zmq.SUB)
-        subscriber.connect("tcp://127.0.0.1:5600")  # Connect to the same port as in your GNU Radio script
+        subscriber.connect("tcp://127.0.0.1:5600")  
         subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
         msj = subscriber.recv_string()
         print(msj)
