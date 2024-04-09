13ab6cf75e133c3b8d7fd641ed71347a04319021
Romina Garcia <rominag@fing.edu.uy>
2024-04-09 15:53:52 -0300

    Update selector_39_impl.cc

--- gr-itais/lib/selector_39_impl.cc
+++ gr-itais/lib/selector_39_impl.cc
@@ -16,6 +16,9 @@
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
+
+ * This file was adapted from the Selector block in GNU Radio 3.9
+
  */
 
 #ifdef HAVE_CONFIG_H
